b1ea7e6116c3322eb3a04721dc471dc1f972da91
Hao Luo <haoluo@google.com>
2020-08-19 17:55:23 -0700

    Test bpf_per_cpu_ptr(). Test two paths in the kernel. If the base pointer points to a struct, the returned reg is of type PTR_TO_BTF_ID. Direct pointer dereference can be applied on the returned variable. If the base pointer isn't a struct, the returned reg is of type PTR_TO_MEM, which also supports direct pointer dereference.
    Signed-off-by: Hao Luo <haoluo@google.com>
    ---
     .../testing/selftests/bpf/prog_tests/ksyms_btf.c  |  4 ++++
     .../testing/selftests/bpf/progs/test_ksyms_btf.c  | 15 ++++++++++++++-
     2 files changed, 18 insertions(+), 1 deletion(-)

--- tools/testing/selftests/bpf/progs/test_ksyms_btf.c
+++ tools/testing/selftests/bpf/progs/test_ksyms_btf.c
@@ -7,15 +7,28 @@
 
 __u64 out__runqueues = -1;
 __u64 out__bpf_prog_active = -1;
+__u32 out__rq_cpu = -1;
+unsigned long out__process_counts = -1;
 
-extern const struct rq runqueues __ksym; /* struct type global var. */
+extern const struct rq runqueues __ksym; /* struct type percpu var. */
 extern const int bpf_prog_active __ksym; /* int type global var. */
+extern const unsigned long process_counts __ksym; /* int type percpu var. */
 
 SEC("raw_tp/sys_enter")
 int handler(const void *ctx)
 {
+	struct rq *rq;
+	unsigned long *count;
+
 	out__runqueues = (__u64)&runqueues;
 	out__bpf_prog_active = (__u64)&bpf_prog_active;
+
+	rq = (struct rq *)bpf_per_cpu_ptr(&runqueues, 1);
+	if (rq)
+		out__rq_cpu = rq->cpu;
+	count = (unsigned long *)bpf_per_cpu_ptr(&process_counts, 1);
+	if (count)
+		out__process_counts = *count;
 
 	return 0;
 }
